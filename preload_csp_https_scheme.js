05e2adf26dd48cbd3c1981af24207061285f7b36
Asa Schachar <asa@optimizely.com>
2016-10-27 23:21:37 -0700

    Add logging for clarity

--- preload_csp_https_scheme.js
+++ preload_csp_https_scheme.js
@@ -3,4 +3,7 @@ const webFrame = require('electron').webFrame;
 // Bypass CSP compliance using protocol scheme
 webFrame.registerURLSchemeAsBypassingCSP('https');
 
+console.log(`[WEBVIEW] Changed CSP Scheme using the scheme 'https'
+Attempting to load script from the following url: 'https://hosted-script.herokuapp.com/alert.js'`);
+
 require('./script');
--- preload_csp_url_scheme.js
+++ preload_csp_url_scheme.js
@@ -3,4 +3,7 @@ const webFrame = require('electron').webFrame;
 // Bypass CSP compliance using url scheme
 webFrame.registerURLSchemeAsBypassingCSP('https://*.herokuapp.com');
 
+console.log(`[WEBVIEW] Changed CSP Scheme using the scheme 'https://*.herokuapp.com'
+Attempting to load script from the following url: 'https://hosted-script.herokuapp.com/alert.js'`);
+
 require('./script');
